56ca0b847eeaea39998954909adb7c9a181d9d5a
Andrew Hamilton <andrew.hamilton@redspace.ca>
2017-03-14 23:36:31 -0300

    updates

--- js/graph.directive.js
+++ js/graph.directive.js
@@ -85,7 +85,7 @@ angular.module('app')
                   })
                   .attr('x', 125)
                   .text(function (d) {
-                    return d.name + "\nHOH Wins: " + d.hoh + " POV wins: " + d.pov;
+                    return d.name + " --- HOH Wins: " + d.hoh + " POV wins: " + d.pov;
                   })
                   .append("tspan")
                   .attr('fill', '#fff')
