b49d912b840560d68960556c40a1f57ab3d709ad
Andrew Hamilton <andrew.hamilton@redspace.com>
2019-01-16 20:09:36 -0400

    prep for celebrity bb 2019

--- js/data.service.js
+++ js/data.service.js
@@ -1,27 +1,27 @@
 //IIFE - keeps code isolated and off global scope
-(function() {
-  angular.module("data", []).service("DataService", DataService);
+;(function() {
+  angular.module("data", []).service("DataService", DataService)
 
   //injection for js minification
-  DataService.$inject = ["$log", "$http"];
+  DataService.$inject = ["$log", "$http"]
 
   //service begins
   function DataService($log, $http) {
-    $log = $log.getInstance("DataService", false);
+    $log = $log.getInstance("DataService", false)
 
     //internal method attached to service
-    this.houseguests = [];
-    this.members = [];
-    this.useFirebase = false;
-    this.teamSize = 5;
-    this.firebaseUrl = "bb-pool.firebaseio.com";
-    this.get = get;
+    this.houseguests = []
+    this.members = []
+    this.useFirebase = true
+    this.teamSize = 3
+    this.firebaseUrl = "bb-pool.firebaseio.com"
+    this.get = get
 
     //internal methods
     function get() {
-      var self = this;
-      return $http.get("/app.json");
+      var self = this
+      return $http.get("/app.json")
     }
   }
   //service ends
-})();
+})()
